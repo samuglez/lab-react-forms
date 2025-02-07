63f1e4008e667cf6e1d05f23ab8b6c5f541fb01b
unknown <samuglez90@gmail.com>
2025-02-07 17:22:32 +0100

    Hecho Samuel

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,138 @@
+// AddStudent.jsx
+import React, { useState } from "react";
+
+function AddStudent({ handleAddStudent }) {
+  const [fullName, setFullName] = useState("");
+  const [image, setImage] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [program, setProgram] = useState("");
+  const [graduationYear, setGraduationYear] = useState(2023);
+  const [graduated, setGraduated] = useState(false);
+
+  const handleFullNameChange = (e) => setFullName(e.target.value);
+  const handleImageChange = (e) => setImage(e.target.value);
+  const handlePhoneChange = (e) => setPhone(e.target.value);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handleProgramChange = (e) => setProgram(e.target.value);
+  const handleGraduationYearChange = (e) => setGraduationYear(e.target.value);
+  const handleGraduatedChange = (e) => setGraduated(e.target.checked);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const newStudent = {
+      fullName,
+      email,
+      phone,
+      program,
+      image,
+      graduationYear,
+      graduated
+    };
+
+    handleAddStudent(newStudent);
+
+    setFullName("");
+    setImage("");
+    setPhone("");
+    setEmail("");
+    setProgram("");
+    setGraduationYear(2023);
+    setGraduated(false);
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <span>Add a Student</span>
+      <div>
+        <label>
+          Full Name
+          <input
+            name="fullName"
+            type="text"
+            placeholder="Full Name"
+            value={fullName}
+            onChange={handleFullNameChange}
+          />
+        </label>
+
+        <label>
+          Profile Image
+          <input
+            name="image"
+            type="url"
+            placeholder="Profile Image"
+            value={image}
+            onChange={handleImageChange}
+          />
+        </label>
+
+        <label>
+          Phone
+          <input
+            name="phone"
+            type="tel"
+            placeholder="Phone"
+            value={phone}
+            onChange={handlePhoneChange}
+          />
+        </label>
+
+        <label>
+          Email
+          <input
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Program
+          <select
+            name="program"
+            value={program}
+            onChange={handleProgramChange}
+          >
+            <option value="">-- None --</option>
+            <option value="Web Dev">Web Dev</option>
+            <option value="UXUI">UXUI</option>
+            <option value="Data">Data</option>
+          </select>
+        </label>
+
+        <label>
+          Graduation Year
+          <input
+            name="graduationYear"
+            type="number"
+            placeholder="Graduation Year"
+            min={2023}
+            max={2030}
+            value={graduationYear}
+            onChange={handleGraduationYearChange}
+          />
+        </label>
+
+        <label>
+          Graduated
+          <input
+            name="graduated"
+            type="checkbox"
+            checked={graduated}
+            onChange={handleGraduatedChange}
+          />
+        </label>
+
+        <button type="submit">Add Student</button>
+      </div>
+    </form>
+  );
+}
+
+export default AddStudent;
